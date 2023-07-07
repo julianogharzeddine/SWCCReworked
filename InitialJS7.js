93a1995982bcb0c120642626ae0b1c1fc168e0e1
julianogharzeddine <135016168+julianogharzeddine@users.noreply.github.com>
2023-07-07 15:11:38 +0300

    Update and rename InitialJS6.js to InitialJS7.js

--- InitialJS7.js
+++ InitialJS7.js
@@ -1,5 +1,5 @@
 var baseURL;   // fetching the base URL
-
+var infoIconURL = "https://srv-k2five/Designer/Runtime/File.ashx?_path=gj%2FkK8xKydFGIaBuABcSOduRX2c%3D%5Cinformation-button.png&_filerequestdata=_4XeJqbaWuJp0syOgJHEA8of-kLRzEyrnXA94YOkjasqBnsOTQgJXJ-Ybt28RDf8-rNsOJTV6GFCRJMwfDiB-T1qyY65WRx0Csth2wTf9JOReKkiiOYspbS7vEwYNJBIywx1kBd-LFpHYtIPS0xUdrkixdScIEVBKIgcyqXW3WD2a1CNZt1TjOmkHTF0prdAe6Kyil_9PHynI0KFBGfxlSpFuMC_LFnUMkZaxgfFrVy1zuKMnYwsZLNdUnn1Fg3F02l-Z5JDdXl-ChygqFDt0QT0TpYjnxCCkjbfYOS8_pU1&_height=32&_width=32&_controltype=image&XSRFToken=4399624675727584330"
 
 $(document).ready(function () {
 
@@ -7,12 +7,12 @@ $(document).ready(function () {
 
     baseURL = window.location.protocol + '//' + window.location.host + '/';
 
-    // Wait for the card-wrapper div to render successfully
 
     // Dynamically generating the service tiles
 
     fetchTiles()
         .then(function (data) {
+            // Wait for the card-wrapper div to render successfully
             waitForWrapperRender(data);
         })
         .catch(function (error) {
@@ -53,6 +53,66 @@ $(document).ready(function () {
         $("#serviceName").val("")
         $("#serviceLink").val("")
     })
+
+
+    function waitForWrapperRender(data) {
+        if ($('#sectionBrowser').length > 0) {
+            renderTiles(data);
+        } else {
+            setTimeout(waitForWrapperRender, 500);
+        }
+    }
+
+    function fetchTiles() {
+
+        return new Promise(function (resolve, reject) {
+            $.ajax({
+                type: 'GET',
+                url: `${baseURL}api/odatav4/v4/sections_SMO`,
+                dataType: 'json',
+                crossDomain: false,
+                beforeSend: function (xhr) {
+                    xhr.setRequestHeader('Authorization', 'Basic ' + window.btoa(unescape(encodeURIComponent("sp_admin" + ':' + "P@ssw0rd"))));
+                    xhr.setRequestHeader("Content-Type", "application/json; charset=UTF-8");
+                    xhr.setRequestHeader("Access-Control-Allow-Origin", "*");
+                },
+                success: function (json_data) {
+                    resolve(json_data.value);
+                },
+                error: function () {
+                    reject('Failed to Load Service Tiles!');
+                }
+            });
+        });
+    }
+
+    function renderTiles(data) {
+
+        $('#sectionBrowser').html('')
+        $('#sectionBrowser').append('<p class="sectionBrowserTitle" id="OurDepartments">أقسامنا المختلفة</p>')
+        $("#sectionBrowser").append("<div id='card-wrapper'></div>")
+
+        data.map((tile) => {
+            $("#card-wrapper").append(`
+          <div class="cardItem" onclick="goTo('${tile.ServiceURL ?? ""}')">
+          <div class="infoIconContainer">
+          <img src="${infoIconURL}"
+            class='infoIcon'>
+          </div>
+          <img src="${tile.ServiceImage}" class='titleImage'>
+          <p class="cardTitle" id='LegalAffairs'>${tile.ServiceNameAR}</p>
+          </div>
+        `)
+        })
+    }
+
+    function goTo(href) {
+        if (href) {
+            window.open(href, "_self")
+        }
+
+    }
+
 
 
     // Translating the Page On Load
@@ -110,61 +170,8 @@ function changeLanguage() {
 
 }
 
-function goTo(href) {
-    window.open(href, "_self")
-}
-
-function renderTiles(data) {
-
-    $('#sectionBrowser').html('')
-    $('#sectionBrowser').append('<p class="sectionBrowserTitle" id="OurDepartments">أقسامنا المختلفة</p>')
-    $("#sectionBrowser").append("<div id='card-wrapper'></div>")
-
-    data.map((tile) => {
-        $("#card-wrapper").append(`
-        <div class="cardItem" onclick="goTo('${tile.ServiceURL}')">
-          <div class="infoIconContainer">
-          <img src="https://srv-k2five/Designer/Runtime/File.ashx?_path=gj%2FkK8xKydFGIaBuABcSOduRX2c%3D%5Cinformation-button.png&_filerequestdata=_4XeJqbaWuJp0syOgJHEA8of-kLRzEyrnXA94YOkjasqBnsOTQgJXJ-Ybt28RDf8-rNsOJTV6GFCRJMwfDiB-T1qyY65WRx0Csth2wTf9JOReKkiiOYspbS7vEwYNJBIywx1kBd-LFpHYtIPS0xUdrkixdScIEVBKIgcyqXW3WD2a1CNZt1TjOmkHTF0prdAe6Kyil_9PHynI0KFBGfxlSpFuMC_LFnUMkZaxgfFrVy1zuKMnYwsZLNdUnn1Fg3F02l-Z5JDdXl-ChygqFDt0QT0TpYjnxCCkjbfYOS8_pU1&_height=32&_width=32&_controltype=image&XSRFToken=4399624675727584330"
-            class='infoIcon'>
-          </div>
-          <img src="${tile.ServiceImage}" class='titleImage'>
-          <p class="cardTitle" id='LegalAffairs'>${tile.ServiceNameAR}</p>
-          </div>
-        `)
-    })
-}
-
-function fetchTiles() {
-
-    return new Promise(function (resolve, reject) {
-        $.ajax({
-            type: 'GET',
-            url: `${baseURL}api/odatav4/v4/sections_SMO`,
-            dataType: 'json',
-            crossDomain: false,
-            beforeSend: function (xhr) {
-                xhr.setRequestHeader('Authorization', 'Basic ' + window.btoa(unescape(encodeURIComponent("sp_admin" + ':' + "P@ssw0rd"))));
-                xhr.setRequestHeader("Content-Type", "application/json; charset=UTF-8");
-                xhr.setRequestHeader("Access-Control-Allow-Origin", "*");
-            },
-            success: function (json_data) {
-                resolve(json_data.value);
-            },
-            error: function () {
-                reject('Failed to Load Service Tiles!');
-            }
-        });
-    });
-}
 
 
-function waitForWrapperRender(data) {
-    if ($('#sectionBrowser').length > 0) {
-        renderTiles(data);
-    } else {
-        setTimeout(waitForWrapperRender, 500);
-    }
-}
 
 function translateToEnglish() {
 
