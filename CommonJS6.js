a61b3e58ef41846645d62220d1d375b67310f0db
julianogharzeddine <135016168+julianogharzeddine@users.noreply.github.com>
2023-08-07 16:23:40 +0300

    Update and rename CommonJS5.js to CommonJS6.js

---
+++ CommonJS6.js
@@ -0,0 +1,47 @@
+var bool = false;
+var currentURL;
+var currentLanguage;
+
+$(document).ready(function () {
+
+    currentURL = window.location.href;
+
+    const runtimeKeyword = "RuntimeAR";
+
+    if (currentURL.includes(runtimeKeyword)) {
+        currentLanguage = "AR"
+    } else {
+        currentLanguage = "EN"
+    }
+
+    $("[name='EnglishFlag']").on("click", function () {
+        updateURL("EnglishFlag");
+    });
+
+    $("[name='ArabicFlag']").on("click", function () {
+        updateURL("ArabicFlag");
+    });
+
+});
+
+// Function to update the URL based on flag clicks
+function updateURL(keyword) {
+
+    let newURL;
+
+    if (keyword === "EnglishFlag") {
+        if (currentURL.includes("RuntimeAR")) {
+            newURL = currentURL.replace("RuntimeAR", "Runtime");
+        }
+
+    } else if (keyword === "ArabicFlag") {
+        if (!currentURL.includes("RuntimeAR")) {
+            newURL = currentURL.replace("Runtime", "RuntimeAR");
+        }
+    }
+
+    // Change the URL and immediately go to the new URL in the same tab
+    if (newURL) {
+        window.location.replace(newURL);
+    }
+}
