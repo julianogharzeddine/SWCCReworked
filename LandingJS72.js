42ee36ae143c0bb88fd6833afb9ec005741e699e
julianogharzeddine <135016168+julianogharzeddine@users.noreply.github.com>
2023-08-18 13:19:57 +0300

    Update and rename LandingJS71.js to LandingJS72.js

--- LandingJS72.js
+++ LandingJS72.js
@@ -30,33 +30,8 @@ $(document).ready(function () {
     // Translating the Page On Load
 
     dictionary = [
-        { "English": "Investigations Management", "Arabic": "إدارة القضايا و التحقيقات", "French": "Aff. Juridiques" },
-        { "English": "Proceed Against Institution", "Arabic": "إجراء ضد مؤسسة", "French": "Procéder Contre Inst." },
-        { "English": "Proceed With Institution", "Arabic": "إجراء مع المؤسسة", "French": "Procéder Avec Inst." },
-        { "English": "Request Investigation", "Arabic": "إجراء تحقيق", "French": "Demander Enquête" },
-        { "English": "Conflict Of Interest Procedure", "Arabic": "إجراء تضارب المصالح", "French": "Procédure Conflit Intérêt" },
-        { "English": "Contract Study Procedures", "Arabic": "إجراءات دراسة العقود", "French": "Procédures Étude Contrats" },
-        { "English": "Click Here", "Arabic": "إضغط هنا", "French": "Cliquer" },
-        { "English": "Conduct Investigation", "Arabic": "إجراء تحقيق", "French": "Mener Enquête" },
-        { "English": "Investigation Requests", "Arabic": "طلبات التحقيق", "French": "Demandes D'Enquête" },
-        { "English": "Submit Complaint", "Arabic": "تقديم شكوى", "French": "Soumettre Plainte" },
-        { "English": "New", "Arabic": "الجديدة", "French": "Nouveau" },
-        { "English": "Active", "Arabic": "النشطة", "French": "Actif" },
-        { "English": "Completed", "Arabic": "المكتملة", "French": "Terminé" },
-        { "English": "Created By", "Arabic": "انشا من قبل", "French": "Créé Par" },
-        { "English": "Investigation Status", "Arabic": "حالة التحقيق", "French": "Statut Enquête" },
-        { "English": "Subject", "Arabic": "الموضوع", "French": "Sujet" },
-        { "English": "out of", "Arabic": "من", "French": "de" },
-        { "English": "Status", "Arabic": "الحالة", "French": "Statut" },
-        { "English": "Purchase", "Arabic": "طلب شراء", "French": "Achat" },
-        { "English": "Sales", "Arabic": "المبيعات", "French": "Ventes" },
-        { "English": "Marketing", "Arabic": "التسويق", "French": "Marketing" },
-        { "English": "Requisitions", "Arabic": "الطلبات", "French": "Demandes" },
-        { "English": "Our Services", "Arabic": "خدماتنا المختلفة", "French": "Nos Services" },
-        { "English": "Purchase No", "Arabic": "رقم الطلب", "French": "Numero" },
-        { "English": "Today", "Arabic": "اليوم", "French": "Auj" },
-        { "English": "Wed", "Arabic": "الأربعاء", "French": "Mer" },
-        { "English": "Thu", "Arabic": "الخميس", "French": "Jeu" }
+        { "English": "", "Arabic": "", "French": "" },
+
     ];
 
 
@@ -117,8 +92,9 @@ $(document).ready(function () {
 
         let categoryName = $(this).find('.categoryName').text()
         let categoryID = $(this).data("cat")
+        let hasSubcategories = isTrue($(this).data("hassubcategories"))
 
-        if (categoryID != 4) {
+        if (hasSubcategories) {
             fetchSubCategoriesJoin()
                 .then(function (data) {
                     renderSubCategoryCards(data, categoryName, categoryID)
@@ -136,15 +112,15 @@ $(document).ready(function () {
         targetRadio.trigger('click')
     })
 
-/* 
-    fetchSubCategoriesJoin()
-        .then(function (data) {
-            renderSubCategoryCards(data, "إدارة القضايا والتحقيقات", 1)
-        })
-        .catch(function (error) {
-            console.error(error);
-        });
- */
+    /* 
+        fetchSubCategoriesJoin()
+            .then(function (data) {
+                renderSubCategoryCards(data, "إدارة القضايا والتحقيقات", 1)
+            })
+            .catch(function (error) {
+                console.error(error);
+            });
+     */
 
 })
 
@@ -239,7 +215,7 @@ function renderInvestCards(data) {
 
         if (containsKeyword) {
             if (investStatus == "All" || targetArray.includes(status)) {
-                $('#card-wrapper').append(`<div class="cardItem"><div class="cardHeader"><div class="investNoStatusWrap"><div class="status" style="background-color: ${redStatus.includes(status) ? "red" : (orangeStatus.includes(status) ? "orange" : (greenStatus.includes(status) ? "green" : "red"))};"></div><div class="investNo"><a href='https://srv-k2five/Runtime/Runtime/Form/RO.Form/?RefNo=${refNo}'>${refNo}</a></div></div><div class='dateWrapper'><div class="date">${new Date(creationDate).toLocaleDateString("en-GB", { day: "2-digit", month: "2-digit", year: "numeric" }).split("/").reverse().join("/")}</div><img src='${dateIconURL}' /></div></div><div class="cardBody"><div class="card-rows"><p class="reqCreator labelVal">${creator}</p><p class="reqCreatorLabel labelTitle translatable">انشا من قبل</p></div><div class="card-rows"><p class="reqCreator labelVal">${status}</p><p class="reqCreatorLabel labelTitle translatable">حالة التحقيق</p></div><div class="card-rows"><p class="reqSubject labelVal">${subject}</p><p class="reqSubjectLabel labelTitle translatable">الموضوع</p></div></div></div>`);
+                $('#card-wrapper').append(`<div class="cardItem"><div class="cardHeader"><div class="investNoStatusWrap"><div class="status" style="background-color: ${redStatus.includes(status) ? "red" : (orangeStatus.includes(status) ? "orange" : (greenStatus.includes(status) ? "green" : "red"))};"></div><div class="investNo"><a href='https://srv-k2five/Runtime/Runtime/Form/RO.Form/?RefNo=${refNo}'>${refNo}</a></div></div><div class='dateWrapper'><div class="date">${new Date(creationDate).toLocaleDateString("en-GB", { day: "2-digit", month: "2-digit", year: "numeric" }).split("/").reverse().join("/")}</div><img src='${dateIconURL}' /></div></div><div class="cardBody"><div class="card-rows"><p class="reqCreator labelVal">${creator}</p><p class="reqCreatorLabel labelTitle">انشا من قبل</p></div><div class="card-rows"><p class="reqCreator labelVal">${status}</p><p class="reqCreatorLabel labelTitle">حالة التحقيق</p></div><div class="card-rows"><p class="reqSubject labelVal">${subject}</p><p class="reqSubjectLabel labelTitle">الموضوع</p></div></div></div>`);
                 filteredResults++
             }
         }
@@ -323,17 +299,17 @@ function renderCounterButtons(data) {
     let content = `
   <div class="Complete counterCard" data-status="Complete">
       <p id="completeCounter" class="counterCircle">${completedNo}</p>
-      <p class="counterLabel translatable">المكتملة</p>
+      <p class="counterLabel">المكتملة</p>
       <p class="totalcounter"><span class='translatable'>من </span> ${totalReq}</p>
   </div>
   <div class="Active counterCard" data-status="Active">
       <p id="activeCounter" class="counterCircle">${activeNo}</p>
-      <p class="counterLabel translatable">النشطة</p>
+      <p class="counterLabel">النشطة</p>
       <p class="totalcounter"><span class='translatable'>من </span> ${totalReq}</p>
   </div>
   <div class="New counterCard" data-status="New">
       <p id="newCounter" class="counterCircle">${newNo}</p>
-      <p class="counterLabel translatable">الجديدة</p>
+      <p class="counterLabel">الجديدة</p>
       <p class="totalcounter"><span class='translatable'>من </span> ${totalReq}</p>
   </div>
   `
@@ -345,21 +321,21 @@ function renderCounterButtons(data) {
 function renderInvestOptions() {
 
     $("#subOptions").find('.sectionBrowserTitle').remove()
-    $("#subOptions").prepend(`<p class="sectionBrowserTitle translatable">إجراء تحقيق</p>`)
+    $("#subOptions").prepend(`<p class="sectionBrowserTitle">إجراء تحقيق</p>`)
 
     $('#InvestigationCards').html("")
     $('#InvestigationCards').append(`
   <div class="cardItem" id='showAllInvestigations'>
       <img src="https://cdn.jsdelivr.net/gh/nourkhansa20/CustomFiles@main/gavel-lawyer-books-isolated-white-justice-law-legal-concept-d-illustration-91106772-transformed.webp" class='titleImage'>
-      <p class="cardTitle translatable">طلبات التحقيق</p>
+      <p class="cardTitle">طلبات التحقيق</p>
   </div>
   <div class="cardItem" onclick="goTo('https://srv-k2five/Runtime/Runtime/Form/Submit.Form/')">
       <img src="https://cdn.jsdelivr.net/gh/nourkhansa20/CustomFiles@main/2000x1000_legal2.jpg" class='titleImage'>
-      <p class="cardTitle translatable">إجراء تحقيق</p>
+      <p class="cardTitle">إجراء تحقيق</p>
   </div>
   <div class="cardItem"  onclick="goTo('https://srv-k2five/Runtime/Runtime/Form/InitialForm.Form/')">
       <img src="https://cdn.jsdelivr.net/gh/nourkhansa20/CustomFiles@main/how-to-file-a-complaint.jpg" class='titleImage'>
-      <p class="cardTitle translatable">تقديم شكوى</p>
+      <p class="cardTitle">تقديم شكوى</p>
   </div>
   
   `)
@@ -369,18 +345,34 @@ function renderInvestOptions() {
 
 function renderSubCategoryCards(data, categoryName, categoryID) {
 
-    $(".sectionBrowserTitle").remove()
-    $('.sectionBrowser').prepend(`<p class="sectionBrowserTitle translatable">${categoryName}</p>`)
-
+    $("#sectionBrowserLandingTitle").remove()
+    $('#sectionBrowser').prepend(`<p class="sectionBrowserLandingTitle">${categoryName}</p>`)
     $('#subcategories-card-wrapper').html("")
-    data.map((item) => {
-        if (item.CategoryID === categoryID) {
 
-            if (item.ID === 3) {
-                $('#subcategories-card-wrapper').append(`<div class="cardItem" id="createInvestigationButton" data-subcat="${item.ID}"><img src="${item.SubCategoryImage}" class='titleImage'><p class="cardTitle translatable">${item.SubCategoryNameAr}</p></div>`)
-            } else {
-                $('#subcategories-card-wrapper').append(`<div class="cardItem" data-subcat="${item.ID}"><img src="${item.SubCategoryImage}" class='titleImage'><p class="cardTitle translatable">${item.SubCategoryNameAr}</p></div>`)
-            }
+    data.map((item) => {
+
+        let subCategoryID = item.ID
+        let subCategoryName = langIsAr() ? item.SubCategoryNameAR :  item.SubCategoryNameEN
+        let subCategoryDesc = langIsAr() ? item.SubCategoryDescriptionAR :  item.SubCategoryDescriptionEN
+        let subCategoryImageURL = item.SubCategoryImageURL
+        let subCategoryURL = item.SubCategoryURL
+        let isActive = isTrue(item.IsActive)
+        let isClickable = isTrue(item.IsClickable)
+        let subCatJSID = item.JavaScriptID
+
+        if (item.CategoryID === categoryID && isActive) {
+            $('#subcategories-card-wrapper').append(`
+ <div class="cardItem subcategoryItem" id="${subCatJSID }" data-subcat="${subCategoryID}" ${isClickable ? `onclick="goTo('${subCategoryURL}')"` : ""}>
+ <div class='imageWrapper'>
+ <img src="${subCategoryImageURL}" class='titleImage' alt="Sub-Service Image.jpeg">
+ </div>
+ <div class='TitleAndDescWrapper'>
+ <p class="cardTitle">${subCategoryName}</p>
+ <p class="serviceDescription"> ${subCategoryDesc} </p>
+ </div>
+ </div>
+ `
+            )
 
         }
 
@@ -388,83 +380,6 @@ function renderSubCategoryCards(data, categoryName, categoryID) {
 
 }
 
-
-function translate() {
-    let LSLang = localStorage.getItem('selected_language')
-    let targetLang = ""
-
-    switch (LSLang) {
-        case 'en-US':
-            targetLang = 'English'
-            $('.taskDD').css('left', '72%')
-            $('[name="Sidebar"]').css('right', '')
-            $('[name="Sidebar"]').css('left', '0')
-            $('.runtime-form').css('left', '')
-            $('.runtime-form').css('left', '20%')
-            $('.counterCard').css('flex-direction', 'row-reverse')
-            $('.dateWrapper').css('flex-direction', 'row-reverse')
-            $('.card-rows').css('flex-direction', 'row-reverse')
-            $('.cardHeader').css('flex-direction', 'row')
-            $('.investNoStatusWrap').css('flex-direction', 'row')
-            $('#subcategories-card-wrapper').css('direction', 'ltr')
-            $('#card-wrapper').css('direction', 'ltr')
-            $('.taskDD a').css('flex-direction', 'row')
-            $('.task-details p').css({
-                'text-align': 'left',
-                'direction': 'rtl'
-            })
-            $(".task-details h4").css("text-align", "left")
-            break
-        case 'ar-SA':
-            targetLang = 'Arabic'
-            $('.taskDD').css('left', '20%')
-            $('[name="Sidebar"]').css('left', '')
-            $('[name="Sidebar"]').css('right', '0')
-            $('[name="Sidebar"]').css('left', '')
-            $('.runtime-form').css('left', '5%')
-            $('.counterCard').css('flex-direction', 'row-reverse')
-            $('.dateWrapper').css('flex-direction', 'row')
-            $('.card-rows').css('flex-direction', 'row-reverse')
-            $('.cardHeader').css('flex-direction', 'row')
-            $('.investNoStatusWrap').css('flex-direction', 'row')
-            $('#subcategories-card-wrapper').css('direction', 'rtl')
-            $('#card-wrapper').css('direction', 'rtl')
-            $('.taskDD a').css('flex-direction', 'row-reverse')
-            $('.task-details p').css({
-                'text-align': 'right',
-                'direction': 'ltr'
-            })
-            $(".task-details h4").css("text-align", "right")
-            break
-        case 'fr-FR':
-            targetLang = 'French'
-            $('.taskDD').css('right', '72%')
-            $('[name="Sidebar"]').css('right', '')
-            $('[name="Sidebar"]').css('left', '0')
-            $('.runtime-form').css('left', '')
-            $('.runtime-form').css('left', '20%')
-            $('.counterCard').css('flex-direction', 'row-reverse')
-            $('.card-rows').css('flex-direction', 'row-reverse')
-            $('.cardHeader').css('flex-direction', 'row')
-            $('.dateWrapper').css('flex-direction', 'row')
-            $('#subcategories-card-wrapper').css('direction', 'ltr')
-            $('#card-wrapper').css('direction', 'ltr')
-            $('.taskDD a').css('flex-direction', 'row')
-            $('.task-details p').css({
-                'text-align': 'left',
-                'direction': 'rtl'
-            })
-            $(".task-details h4").css("text-align", "left")
-            break
-    }
-
-    let toTranslate = $('.translatable')
-
-    toTranslate.each(function () {
-        $(this).text(getFromDictionary(($(this).text().trim()), targetLang))
-    })
-
-}
 
 function getFromDictionary(text, toLanguage) {
     for (var i = 0; i < dictionary.length; i++) {
