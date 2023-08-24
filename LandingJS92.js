ff7cd6e275d251e5a1a5397bf82fee86649f1fc8
julianogharzeddine <135016168+julianogharzeddine@users.noreply.github.com>
2023-08-24 11:20:13 +0300

    Update and rename LandingJS91.js to LandingJS92.js

--- LandingJS92.js
+++ LandingJS92.js
@@ -332,7 +332,7 @@ function waitForCounterWrapperRender(data) {
 // Rendering Investigation Cards
 function renderInvestCards(data) {
 
-    $('#card-wrapper').html("")
+    $('#investigations-card-wrapper').html("")
     let filteredResults = 0;
 
     data.map((investigation) => {
@@ -367,7 +367,65 @@ function renderInvestCards(data) {
 
         if (containsKeyword) {
             if (investStatus == "All" || targetArray.includes(status)) {
-                $('#card-wrapper').append(`<div class="cardItem"><div class="cardHeader"><div class="investNoStatusWrap"><div class="status" style="background-color: ${redStatus.includes(status) ? "red" : (orangeStatus.includes(status) ? "orange" : (greenStatus.includes(status) ? "green" : "red"))};"></div><div class="investNo"><a href='https://srv-k2five/Runtime/Runtime/Form/RO.Form/?RefNo=${refNo}'>${refNo}</a></div></div><div class='dateWrapper'><div class="date">${new Date(creationDate).toLocaleDateString("en-GB", { day: "2-digit", month: "2-digit", year: "numeric" }).split("/").reverse().join("/")}</div><img src='${dateIconURL}' /></div></div><div class="cardBody"><div class="card-rows"><p class="reqCreator labelVal">${creator}</p><p class="reqCreatorLabel labelTitle">انشا من قبل</p></div><div class="card-rows"><p class="reqCreator labelVal">${status}</p><p class="reqCreatorLabel labelTitle">حالة التحقيق</p></div><div class="card-rows"><p class="reqSubject labelVal">${subject}</p><p class="reqSubjectLabel labelTitle">الموضوع</p></div></div></div>`);
+                $('#investigations-card-wrapper').append(`
+                <div class="investigation-card">
+            <div class="investigationHeader">
+                <div class="investigationRefNo">
+                    <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" stroke="#0066cc"
+                        stroke-width="1.152">
+                        <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+                        <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round" stroke="#CCCCCC"
+                            stroke-width="0.048"></g>
+                        <g id="SVGRepo_iconCarrier">
+                            <path
+                                d="M15.7281 3.88396C17.1624 2.44407 19.2604 2.41383 20.4219 3.57981C21.5856 4.74798 21.5542 6.85922 20.1189 8.30009L17.6951 10.7333C17.4028 11.0268 17.4037 11.5017 17.6971 11.794C17.9906 12.0863 18.4655 12.0854 18.7578 11.7919L21.1816 9.35869C23.0929 7.43998 23.3329 4.37665 21.4846 2.5212C19.6342 0.663551 16.5776 0.905664 14.6653 2.82536L9.81768 7.69182C7.90639 9.61053 7.66643 12.6739 9.5147 14.5293C9.80702 14.8228 10.2819 14.8237 10.5754 14.5314C10.8688 14.2391 10.8697 13.7642 10.5774 13.4707C9.41376 12.3026 9.4451 10.1913 10.8804 8.75042L15.7281 3.88396Z"
+                                fill="#0066cc"></path>
+                            <path opacity="0.5"
+                                d="M14.4846 9.4707C14.1923 9.17724 13.7174 9.17632 13.4239 9.46864C13.1305 9.76097 13.1296 10.2358 13.4219 10.5293C14.5856 11.6975 14.5542 13.8087 13.1189 15.2496L8.27129 20.1161C6.83696 21.556 4.73889 21.5862 3.57742 20.4202C2.41376 19.2521 2.4451 17.1408 3.8804 15.6999L6.30424 13.2666C6.59657 12.9732 6.59565 12.4983 6.30219 12.206C6.00873 11.9137 5.53386 11.9146 5.24153 12.208L2.81769 14.6413C0.906387 16.56 0.666428 19.6234 2.5147 21.4788C4.36518 23.3365 7.42173 23.0944 9.334 21.1747L14.1816 16.3082C16.0929 14.3895 16.3329 11.3262 14.4846 9.4707Z"
+                                fill="#0066cc"></path>
+                        </g>
+                    </svg>
+                    <a href="">${refNo}</a>
+                </div>
+                <div class="investigationStatus">
+                    Medium
+                </div>
+            </div>
+            <hr>
+            <div class="investigationBody">
+                <p class="subjectTitle">Subject</p>
+                <p class="subjectParagraph">
+                ${subject}
+                </p>
+            </div>
+            <hr>
+            <div class="investigationFooter">
+                <div class="authorWrapper">
+                    <svg fill="#000000" height="22px" width="22px" viewBox="0 0 24 24" id="edit-user"
+                        data-name="Flat Color" xmlns="http://www.w3.org/2000/svg" class="icon flat-color">
+                        <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+                        <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
+                        <g id="SVGRepo_iconCarrier">
+                            <path id="primary"
+                                d="M6,7a5,5,0,1,1,5,5A5,5,0,0,1,6,7Zm5,12.61a2,2,0,0,1,.59-1.41l4-4A6.26,6.26,0,0,0,14,14H8a6,6,0,0,0-6,6,2,2,0,0,0,2,2h6a1,1,0,0,0,1-1Z"
+                                style="fill: #0066cc000000;"></path>
+                            <path id="secondary"
+                                d="M20.31,12.29l1.4,1.4a1,1,0,0,1,0,1.4L15.1,21.71a1.05,1.05,0,0,1-.71.29H13a1,1,0,0,1-1-1V19.61a1.05,1.05,0,0,1,.29-.71l6.62-6.61A1,1,0,0,1,20.31,12.29Z"
+                                style="fill: #0066cc;"></path>
+                        </g>
+                    </svg>
+                    <div class="graySeparator"></div>
+                    <p class="authorName">${creator}</p>
+                </div>
+                <div class="dateWrapper">
+                    <p class="investigationDate">
+                    ${new Date(creationDate).toLocaleDateString("en-GB", { day: "2-digit", month: "2-digit", year: "numeric" }).split("/").reverse().join("/")}
+                    </p>
+                </div>
+            </div>
+        </div>
+              
+`);
                 filteredResults++
             }
         }
