d28942d4af4880563bbf51870e30713016bbaa1e
julianogharzeddine <135016168+julianogharzeddine@users.noreply.github.com>
2023-08-29 13:20:08 +0300

    Update and rename WorklistJS.js to WorklistJS1.js

---
+++ WorklistJS1.js
@@ -0,0 +1,31 @@
+$(document).ready(function () {
+
+    $('.taskStatusOption').click(function () {
+
+        // Clearing other underlined options
+        $(".underline").css("width", "0")
+
+        // Adding the underline to the current option
+        $(this).find('.underline').css("width", "100%")
+
+        // Unselecting the previously selected option
+        $(".taskStatusOption").removeClass("selectedOption")
+
+        // Reselecting the CURRENT option
+        $(this).addClass("selectedOption")
+    })
+
+    waitForWorklistWrapperRender()
+})
+
+function waitForWorklistWrapperRender() {
+    if ($('#worklist-items-wrapper').length > 0) {
+        renderWorklist()
+    } else {
+        setTimeout(waitForWorklistWrapperRender, 500);
+    }
+}
+
+function renderWorklist() {
+    $("#worklist-items-wrapper").append("<p> Hello </p>")
+} w
